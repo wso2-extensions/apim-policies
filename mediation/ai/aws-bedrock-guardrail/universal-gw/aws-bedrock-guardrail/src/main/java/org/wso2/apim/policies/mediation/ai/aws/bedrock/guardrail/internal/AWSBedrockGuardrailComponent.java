dc1414fc9e3b42744527b6f2c4cf828a4ea0a231
Nimsara Fernando <nimsara66@gmail.com>
2025-07-20 23:22:31 +0530

    Add AWS Bedrock Guardrail README.md

--- mediation/ai/aws-bedrock-guardrail/universal-gw/aws-bedrock-guardrail/src/main/java/org/wso2/apim/policies/mediation/ai/aws/bedrock/guardrail/internal/AWSBedrockGuardrailComponent.java
+++ mediation/ai/aws-bedrock-guardrail/universal-gw/aws-bedrock-guardrail/src/main/java/org/wso2/apim/policies/mediation/ai/aws/bedrock/guardrail/internal/AWSBedrockGuardrailComponent.java
@@ -1,13 +1,12 @@
 /*
+ * Copyright (c) 2025, WSO2 LLC. (http://www.wso2.com).
  *
- * Copyright (c) 2025 WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
- *
- * WSO2 Inc. licenses this file to you under the Apache License,
+ * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -15,7 +14,6 @@
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- *
  */
 
 package org.wso2.apim.policies.mediation.ai.aws.bedrock.guardrail.internal;
